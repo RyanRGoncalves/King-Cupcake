68a0e07a20c114366637969d1e4d7f66033af438
fernando <fernando.lopes48@etec.sp.gov.br>
2025-08-29 15:29:24 -0300

    adição do h1

--- form.php
+++ form.php
@@ -1,10 +1,11 @@
 <?php
 include __DIR__.'/includes/header.php';
 ?>
+<h1 style="color: white">Contate-nos</h1>
 <form action="" method="post">
-    <input type="text" name="nome" placeholder="Nome" style="width: 326px"><br>
-    <input type="email" name="email" placeholder="Email" style="width: 326px"><br>
-    <input type="text" name="descricao" placeholder="Descrição" style="width: 326px" class="des"><br>
+    <input type="text" name="nome" placeholder="Nome" style="width: 800px"><br>
+    <input type="email" name="email" placeholder="Email" style="width: 800px"><br>
+    <input type="text" name="descricao" placeholder="Descrição" style="width: 800px" class="des"><br>
     <input type="submit">
 </form>
 <?php
