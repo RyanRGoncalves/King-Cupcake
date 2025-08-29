9031ddd238a1e6beadd12742ecfab91da4eb50cf
fernando <fernando.lopes48@etec.sp.gov.br>
2025-08-29 14:18:39 -0300

    mudanças no footer e header e criação do forms

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kingcake</title>
+</head>
+<body>
+   <header><h1>KingCake</h1></header>
