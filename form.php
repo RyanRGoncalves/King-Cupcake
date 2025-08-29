109ea352d3844c8ccaaabd81a59ac0cafbcb4ace
fernando <fernando.lopes48@etec.sp.gov.br>
2025-08-29 15:17:14 -0300

    mudanças forms

--- form.php
+++ form.php
@@ -4,7 +4,8 @@ include __DIR__.'/includes/header.php';
 <form action="" method="post">
     <input type="text" name="nome" placeholder="Nome" style="width: 326px"><br>
     <input type="email" name="email" placeholder="Email" style="width: 326px"><br>
-    <input type="text" name="descricao" placeholder="descrição" style="width: 326px" style="height: 400px">
+    <input type="text" name="descricao" placeholder="Descrição" style="width: 326px" class="des"><br>
+    <input type="submit">
 </form>
 <?php
 include __DIR__.'/includes/footer.php';
