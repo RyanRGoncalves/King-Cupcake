a407956d3d1ddfcd0c05c4cb1e0fde9865583567
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-09-05 15:02:03 -0300

    Adicionando mais informações para a pagina inicial

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php
-include __DIR__.'/includes/header.php'
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
 <link rel="stylesheet" href="css/index.css">
 <h1>KING CAKES</h1>
 <div class="pag1">
@@ -15,12 +21,12 @@ Baunilha;
     
     <div>
         <h2>Introdução</h2>
-        <p>Somos um pequeno grupo criando Cupcakes para a população!
-Se quiser saber mais, clique <a href="contato.php">aqui.</a></p>
+        <p>Somos um pequeno grupo criando Cupcakes para a população! </p>
     </div>
     
     <div>
         <h2>Contatos</h2>
+        <p>Entre em contato conosco usando Email, Telefone ou presentemente por <a href="contato.php">está</a> pagina!</p>
     </div>
 </div>
 <?php
