8e340b1e951b4f337b5449d98310bec7b35cf23c
Fernando <fernando.lopes48@etec.sp.gov>
2025-09-05 14:55:00 -0300

    Mudanças drasticas

--- form.php
+++ form.php
@@ -1,12 +1,12 @@
 <?php
 include __DIR__.'/includes/header.php';
 ?>
-<h1 style="color: white">Contate-nos</h1>
+<h1 style="color: white; font-size: 40px">Contate-nos</h1>
 <form action="" method="post">
-    <input type="text" name="nome" placeholder="Nome" style="width: 800px"><br>
-    <input type="email" name="email" placeholder="Email" style="width: 800px"><br>
-    <input type="text" name="descricao" placeholder="Descrição" style="width: 800px; height: 200px"><br>
-    <input type="submit">
+    <input type="text" name="nome" placeholder="Nome" style="width: 800px; height: 20px"><br>
+    <input type="email" name="email" placeholder="Email" style="width: 800px; height: 20px"><br>
+    <input type="text" name="descricao" placeholder="Descrição" style="width: 800px; height: 300px"><br>
+    <input type="submit" style="width: 100px">
 </form>
 <?php
 include __DIR__.'/includes/footer.php';
