b874388a443d3033763543f73fd25f762fbef92e
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-09-05 14:19:57 -0300

    Divorcio do CSS

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
 include __DIR__.'/includes/header.php'
 ?>
-
-
-testestestestestestestestestestesteste
+<link rel="stylesheet" href="css/index.css">
+<h1>KING CAKES</h1>
+<div class="pag1">
+    <div>
+        <h2>Cardapio</h2>
+        <pre style="text-align: center; font-size: 20px; margin-top: 80px">Temos:
+Morango;
+Chocolate;
+Baunilha;
+Entre outros!</pre>
+    </div>
+    
+    <div>
+        <h2>Introdução</h2>
+        <p>Somos um pequeno grupo criando Cupcakes para a população!
+Se quiser saber mais, clique aqui.</p>
+    </div>
+    
+    <div>
+        <h2>Contatos</h2>
+    </div>
+</div>
 <?php
 include __DIR__.'/includes/footer.php'
 ?>
